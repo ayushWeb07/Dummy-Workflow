a7909d5c8f95a2f89201aca60599c5b347cf924a
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-17 18:44:24 +0530

    feat: Update code

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Porshe 911 GT3RS!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example funny app listening on port ${port}`)
 })
